3290b8c05dd79da7b7d1eccde58af9a0e7d67c89
Mathilda <me@mathilda.dev>
2021-08-30 16:51:37 -0400

    Create profile & Create uuid

---
+++ uuid.sh
@@ -0,0 +1,21 @@
+# Fetch UUIDs from https://www.uuidgenerator.net
+uuid () {
+
+    re='^[0-9]*$'
+    if [[ $1 =~ $re ]] ; then
+
+        count="${1:-1}"
+
+        if [ $count -le 500 ] ; then
+
+            f_count=$( printf "%04d" "$count" )
+            curl "https://www.uuidgenerator.net/api/version4/${count}"
+            printf '\n'
+
+        else
+            printf '\033[0;31mError: Count may not exceed 500\n'
+        fi
+    else
+        printf '\033[0;31mError: Argument must be an integer\n'
+    fi
+}
