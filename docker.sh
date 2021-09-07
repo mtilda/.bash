08af1b4ad696dbb0c21cc83d321ee85fb13481b0
Mathilda <me@mathilda.dev>
2021-09-07 11:47:12 -0400

    Add docker-wipe

---
+++ docker.sh
@@ -0,0 +1,9 @@
+# Remove:
+#   - all stopped containers
+#   - all networks not used by at least one container
+#   - all volumes not used by at least one container
+#   - all images without at least one container associated to them
+#   - all build cache
+# 
+# Answer y to the warning prompt
+docker-wipe () { docker system prune -a --volumes -f; }
