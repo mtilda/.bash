ff5fda30e349bf55777e31ce938e16a5f2c47ab1
Mathilda <me@mathilda.dev>
2021-09-07 16:42:38 -0400

    docker-psql-query

--- docker.sh
+++ docker.sh
@@ -24,7 +24,7 @@ docker-psql-wipe () {
     "
 }
 
-# List all visible tables in database
+# List all visible tables in PostgreSQL database
 docker-psql-tables () {
     echo "mtilda: Listing all tables in PostgreSQL database '${1:-postgres}' in container '${2:-db}'"
 
@@ -33,4 +33,15 @@ docker-psql-tables () {
 
     echo "mtilda: docker-compose exec ${2:-db} psql -U ${1:-postgres} -c \"\d\""
     docker-compose exec ${2:-db} psql -U ${1:-postgres} -c "\d"
+}
+
+# Run any query in PostgreSQL database
+docker-psql-query () {
+    echo "mtilda: Executing query in PostgreSQL database '${2:-postgres}' in container '${3:-db}'"
+
+    echo "mtilda: docker-compose up -d ${3:-db}"
+    docker-compose up -d ${3:-db}
+
+    echo "mtilda: docker-compose exec ${2:-db} psql -U ${2:-postgres} -c \"...\""
+    docker-compose exec ${2:-db} psql -U ${2:-postgres} -c "${1}"
 }
