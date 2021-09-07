052cffbfa8a1c5af86f630174d2190c69662a165
Mathilda <me@mathilda.dev>
2021-09-07 16:31:02 -0400

    Add docker-psql-wipe

--- docker.sh
+++ docker.sh
@@ -7,3 +7,19 @@
 # 
 # Answer y to the warning prompt
 docker-destroy () { docker system prune -a --volumes -f; }
+
+# Drop all tables in database
+docker-psql-wipe () {
+    echo "mtilda: Dropping all tables in PostgreSQL database '${1:-postgres}' in container '${2:-db}'"
+
+    echo "mtilda: docker-compose up -d ${2:-db}"
+    docker-compose up -d ${2:-db}
+    
+    echo "mtilda: docker-compose exec ${2:-db} psql -U ${1:-postgres} -c '...'"
+    docker-compose exec ${2:-db} psql -U ${1:-postgres} -c " \
+    DROP SCHEMA public CASCADE; \
+    CREATE SCHEMA public; \
+    GRANT ALL ON SCHEMA public TO postgres; \
+    GRANT ALL ON SCHEMA public TO public;
+    "
+}
