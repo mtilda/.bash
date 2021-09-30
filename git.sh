07ee8ef79dde084b343d84422cb6710ea72c6d36
Mathilda <me@mathilda.dev>
2021-09-29 23:50:00 -0400

    Fix style and git bugs

--- git.sh
+++ git.sh
@@ -1,14 +1,14 @@
 # Return the current git branch
 # Example: "main"
 gbranch () {
-  echo "$(printf "\033[32m$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')")"
+  echo "$(printf "$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')")"
 }
 
 # Return the current git branch in parentheses
 # Example: "(main)"
 gbranchf () {
   if [[ $(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/') ]]; then
-    echo "$(printf "\033[00m($(gbranch)\033[00m)")"
+    echo "$(printf "\033[00m(\033[32m$(gbranch)\033[00m)")"
   else
     echo
   fi
--- profile
+++ profile
@@ -4,9 +4,11 @@ mbash="$( cd "$( dirname "${BASH_SOURCE}" )" &> /dev/null && pwd )"
 # Init
 . $mbash/intro.sh
 . $mbash/manage.sh
-. $mbash/style.sh
 
 # Main
 . $mbash/docker.sh
 . $mbash/git.sh
 . $mbash/uuid.sh
+
+# Style
+. $mbash/style.sh
