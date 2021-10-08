808647a41b0dc7d40f8d57682ca262f7dffe5951
Mathilda <me@mathilda.dev>
2021-10-08 01:02:44 -0400

    Refactor docker commands

--- docker.sh
+++ docker.sh
@@ -8,17 +8,26 @@ alias doc="docker-compose"
 #   - all build cache
 # 
 # Answer y to the warning prompt
-docker-destroy () { docker system prune -a --volumes -f; }
+docker-destroy () {
+    echo "mtilda: Destroy all expendable docker entities"
+    
+    docker system prune -a --volumes -f;
+}
+
+# Run any query in PostgreSQL database container
+docker-psql () {
+    echo "mtilda: Execute query in PostgreSQL database '${2:-postgres}' in container '${3:-db}'"
+
+    docker-compose up -d ${3:-db}
+    docker-compose exec ${2:-db} psql -U ${2:-postgres} -c "${1}"
+}
 
 # Drop all tables in database
 docker-psql-wipe () {
-    echo "mtilda: Dropping all tables in PostgreSQL database '${1:-postgres}' in container '${2:-db}'"
+    echo "mtilda: Drop and recreate schema in PostgreSQL database '${1:-postgres}' in container '${2:-db}'"
 
-    echo "mtilda: docker-compose up -d ${2:-db}"
-    docker-compose up -d ${2:-db}
-    
-    echo "mtilda: docker-compose exec ${2:-db} psql -U ${1:-postgres} -c '...'"
-    docker-compose exec ${2:-db} psql -U ${1:-postgres} -c " \
+    # Drop all tables
+    docker-psql " \
     DROP SCHEMA public CASCADE; \
     CREATE SCHEMA public; \
     GRANT ALL ON SCHEMA public TO postgres; \
@@ -28,22 +37,7 @@ docker-psql-wipe () {
 
 # List all visible tables in PostgreSQL database container
 docker-psql-tables () {
-    echo "mtilda: Listing all tables in PostgreSQL database '${1:-postgres}' in container '${2:-db}'"
+    echo "mtilda: List all tables in PostgreSQL database '${1:-postgres}' in container '${2:-db}'"
 
-    echo "mtilda: docker-compose up -d ${2:-db}"
-    docker-compose up -d ${2:-db}
-
-    echo "mtilda: docker-compose exec ${2:-db} psql -U ${1:-postgres} -c \"\d\""
-    docker-compose exec ${2:-db} psql -U ${1:-postgres} -c "\d"
-}
-
-# Run any query in PostgreSQL database container
-docker-psql () {
-    echo "mtilda: Executing query in PostgreSQL database '${2:-postgres}' in container '${3:-db}'"
-
-    echo "mtilda: docker-compose up -d ${3:-db}"
-    docker-compose up -d ${3:-db}
-
-    echo "mtilda: docker-compose exec ${2:-db} psql -U ${2:-postgres} -c \"...\""
-    docker-compose exec ${2:-db} psql -U ${2:-postgres} -c "${1}"
+    docker-psql "\d"
 }
