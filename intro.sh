1f4419778ef92ed48040555ca36265077f690313
Mathilda <me@mathilda.dev>
2021-10-17 23:47:35 -0400

    Change intro

--- intro.sh
+++ intro.sh
@@ -1,15 +1,8 @@
 # Message displayed on terminal start
-
-# If this is the first time this script is sourced,
-#   Change to home directory
-[[ -z "$SHELL_INITIALIZED" ]] && cd
-SHELL_INITIALIZED=1
-
 clear
 COLS=$(tput cols)
 for (( n=1; n<=$COLS/6; n++ ))
 do
   printf "🚀 🛸 "
 done
-echo
 echo
