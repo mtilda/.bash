cddcabbb85577fe51377e1de9d69b3034e1fe26d
Mathilda Grace <me@mathilda.dev>
2025-02-26 01:38:44 -0500

    Refactor `uuid` to not rely on the internet

--- uuid.sh
+++ uuid.sh
@@ -1,23 +1,33 @@
-# Fetch UUIDs from https://www.uuidgenerator.net
+#!/bin/bash
+
+# Do whatever is necessary to get the specified number of Version 4 UUIDs
 uuid () {
 
   REGEX='^[0-9]*$'
-  
-  if [[ $1 =~ $REGEX ]] ; then
+  if ! [[ $1 =~ $REGEX ]]
+  then
+    printf '\033[0;31mError: Argument must be a non-negative integer.\n'
+  fi
 
-    count="${1:-1}"
+  count="${1:-1}"
 
-    if [ $count -le 500 ] ; then
+  if command -v uuidgen 2>&1 >/dev/null
+  then
+    for i in $(seq $count)
+    do
+      uuidgen
+    done
+  else
+    printf '\033[0;33mWarning: uuidgen not be found. Fetching UUIDs from internet.\033[0m\n' >&2
 
-      f_count=$( printf "%04d" "$count" )
-      curl "https://www.uuidgenerator.net/api/version4/${count}"
-      printf '\n'
-
-    else
-      printf '\033[0;31mError: Count may not exceed 500\n'
+    if ! [ $count -le 500 ]
+    then
+      printf '\033[0;31mError: Count may not exceed 500 while fetching UUIDs from the internet.\n'
     fi
 
-  else
-    printf '\033[0;31mError: Argument must be an integer\n'
+    f_count=$( printf "%04d" "$count" )
+    curl "https://www.uuidgenerator.net/api/version4/${count}"
+    printf '\n'
   fi
+
 }
